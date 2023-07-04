1cd924cd3b519c6d00a1cfe7037d5a960f9f16c7
Mirna Perdomo <mperdomo1010@gmail.com>
2023-07-03 22:28:30 -0600

    Actualizando el primer menu

--- src/Principal.java
+++ src/Principal.java
@@ -1,8 +1,19 @@
+import javax.swing.JOptionPane;
 
 public class Principal {
 
 		public static void main(String[] args) {
-			System.out.println("Hola Mundo");
+			/*String[] opcion= {JOptionPane.showInputDialog(null,"Seleccione una Opcion de Conversion","Menu",JOptionPane.PLAIN_MESSAGE,null,
+					new Object[] {"Conversor de Moneda", "Conversor de Pesos"},"Selecciona").toString()};*/
+			
+			String opciones[]= {"Conversor de Moneda", "Conversor de Pesos"};
+			
+			String opc= (String) JOptionPane.showInputDialog(null,"Seleccione una Opcion de Conversion","Menu",JOptionPane.DEFAULT_OPTION,null,opciones,opciones[0]);
+			JOptionPane.showMessageDialog(null, opc);
+			
+			
+			
+			 
 	
 	}
 
