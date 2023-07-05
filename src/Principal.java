195ac50ffdc3de5b8fe577be86827aedb587fb84
Mirna Perdomo <mperdomo1010@gmail.com>
2023-07-04 22:43:26 -0600

    Actualizando el segundo menu

--- src/Principal.java
+++ src/Principal.java
@@ -1,20 +1,36 @@
 import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
 
 public class Principal {
 
 		public static void main(String[] args) {
-			/*String[] opcion= {JOptionPane.showInputDialog(null,"Seleccione una Opcion de Conversion","Menu",JOptionPane.PLAIN_MESSAGE,null,
-					new Object[] {"Conversor de Moneda", "Conversor de Pesos"},"Selecciona").toString()};*/
 			
-			String opciones[]= {"Conversor de Moneda", "Conversor de Pesos"};
-			
-			String opc= (String) JOptionPane.showInputDialog(null,"Seleccione una Opcion de Conversion","Menu",JOptionPane.DEFAULT_OPTION,null,opciones,opciones[0]);
-			JOptionPane.showMessageDialog(null, opc);
-			
-			
-			
+			Object seleccion = JOptionPane.showInputDialog(
+					   null,
+					   "Seleccione una Opcion de Conversion",
+					   "Menu",
+					   JOptionPane.QUESTION_MESSAGE,
+					   null,  // null para icono defecto
+					   new Object[] { "Conversor de Moneda", "Conversor de Pesos" }, 
+					   null);
+
+					System.out.println("El usuario ha elegido "+seleccion);
 			 
-	
+					if (seleccion=="Conversor de Moneda"){
+						int cant = Integer.parseInt(JOptionPane.showInputDialog("Ingrese Cantidad de dinero que desea convertir"));
+						
+						Object conversion = JOptionPane.showInputDialog(
+								   null,
+								   "Elige la moneda a la que deseas convertir tu dinero",
+								   "Moneda",
+								   JOptionPane.QUESTION_MESSAGE,
+								   null,  // null para icono defecto
+								   new Object[] { "De Dolar a Quetzales", "De Dolar a Peso Mexicano", "De Dolar a Libras", "De Dolar a Euros"}, 
+								   null);
+						
+			        }else {
+			            System.out.println("Conversor de Pesos");
+			        }
 	}
 
 }
